dfe3a1bbe789b64090be76260ab8b04be323edeb
Haram Jeong <91401364+haramj@users.noreply.github.com>
2026-03-25 15:48:58 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- C++17/백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ C++17/백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,3 +1,4 @@
+// 1초 후에 X-1 또는 X+1로 이동하게 된다. 순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -5,45 +6,43 @@
 
 using namespace std;
 
-typedef pair<int, int> info;
+typedef pair<int, int> info; // 위치, 현재까지 이동 횟수
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N, K; // 0 ~ 100,000
-    cin >> N >> K;
+    int s, e;
+    cin >> s >> e;
 
-    queue<info> q;
     vector<bool> visited(100001, false);
+    queue<info> q;
 
-    q.push(info(N, 0));
-    visited[N] = true;
+    q.push(info(s, 0));
+    visited[s] = true;
 
     while (!q.empty()) {
-        info cur = q.front();// 위치, 현재까지의 시간.
+        info cur = q.front();
         q.pop();
-
-        if (cur.first == K) {
+        if (cur.first == e) {
             cout << cur.second;
             return 0;
         }
 
-        if (cur.first - 1 >= 0 && !visited[cur.first - 1]) {
-            q.push(info(cur.first - 1, cur.second + 1));
+        if (cur.first - 1 >= 0 && cur.first - 1 <= 100001 && !visited[cur.first - 1]) {
             visited[cur.first - 1] = true;
+            q.push(info(cur.first - 1, cur.second + 1));
         }
-
-        if (cur.first + 1 < 100001 && !visited[cur.first + 1]) {
-            q.push(info(cur.first + 1, cur.second + 1));
+        if (cur.first + 1 >= 0 && cur.first + 1 <= 100001 && !visited[cur.first + 1]) {
             visited[cur.first + 1] = true;
+            q.push(info(cur.first + 1, cur.second + 1));
         }
-
-        if (cur.first * 2 < 100001 && !visited[cur.first * 2]) {
-            q.push(info(cur.first * 2, cur.second + 1));
+        if (cur.first * 2 >= 0 && cur.first * 2 <= 100001 && !visited[cur.first * 2]) {
             visited[cur.first * 2] = true;
+            q.push(info(cur.first * 2, cur.second + 1));
         }
     }
+
     return 0;
 }
